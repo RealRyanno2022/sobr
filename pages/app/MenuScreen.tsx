4588684bea585fe9d00839ef5070c005f54ee679
Daniel Ryan <112699681+RealRyanno2022@users.noreply.github.com>
2024-06-13 17:06:56 +0100

    todo added, working on clocks

--- pages/app/MenuScreen.tsx
+++ pages/app/MenuScreen.tsx
@@ -6,6 +6,12 @@ import MenuSign from '../../components/MenuSign';
 import GradientBackground from '../../components/GradientBackground';
 import MenuButtons from '../../components/MenuButtons';
 
+
+
+// TODO: Fix the error that prevents page rendering
+
+
+
 const MenuScreen: React.FC = () => {
   return (
     <View style={globalStyles.container}>
